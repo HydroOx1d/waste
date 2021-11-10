df6b9629ee14ce121285b3482a3875e397927f73
HydroOx1d <n-esenov44@mail.ru>
2021-11-10 15:22:39 +0600

    small changes

--- js/index.js
+++ js/index.js
@@ -1,18 +1,16 @@
 const form_show = document.querySelector(".content__show")
 const content_show = document.querySelector('.content__show')
 const content_form = document.querySelector('.content__form')
-//IMG
-const form_show_img = document.createElement('img')
-form_show_img.src = './img/close.png'
+const content_show_img = document.querySelector('.content__show img')
 
 form_show.addEventListener("click", showForm)
 
 function showForm() {
   content_form.classList.toggle('show-from')
   if(content_form.classList.contains('show-from')){
-    content_show.appendChild(form_show_img)
+    content_show_img.src = './img/close.png'
   } else {
-    content_show.removeChild(form_show_img)
+    content_show_img.src = './img/menu.png'
   }
 }
 
